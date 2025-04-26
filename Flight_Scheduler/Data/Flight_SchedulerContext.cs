0f2b10dcf867bb062db292a2c9e6daa04dc59469
arsenovb <b_arsenov@abv.bg>
2025-04-26 14:54:16 +0300

    Added Flight and Airline Tables to Db

---
+++ Flight_Scheduler/Data/Flight_SchedulerContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Flight_Scheduler.Models;
+
+namespace Flight_Scheduler.Data
+{
+    public class Flight_SchedulerContext : DbContext
+    {
+        public Flight_SchedulerContext (DbContextOptions<Flight_SchedulerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Flight_Scheduler.Models.Airline> Airline { get; set; } = default!;
+        public DbSet<Flight_Scheduler.Models.Flight> Flight { get; set; } = default!;
+    }
+}
---
+++ Flight_Scheduler/Models/Flight.cs
@@ -0,0 +1,14 @@
+﻿namespace Flight_Scheduler.Models
+{
+    public class Flight
+    {
+        public int Id { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public string Gate { get; set; }
+
+        public int AirlineId {  get; set; }
+
+        public virtual Airline? Airlines { get; set; }
+    }
+}
